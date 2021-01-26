4d5d09821114e9eb4a277d74defc36c61b44bb18
Giacomo <fordprefect96@gmail.com>
2021-01-26 15:56:53 +0100

    fix buildscript

---
+++ src/main/java/it.fordprefect/Main.java
@@ -0,0 +1,12 @@
+package it.fordprefect;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Whatever");
+        JFrame f = new JFrame();
+        f.setVisible(true);
+
+    }
+}
