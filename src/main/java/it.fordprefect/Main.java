f54cc56d01d6ef6571ea796e03d148f7f81c1c54
Giacomo <fordprefect96@gmail.com>
2021-01-27 16:33:36 +0100

    inizio sistema per trovare accenti
    serve per capire se il verso è piano, sdrucciolo o tronco

--- src/main/java/it.fordprefect/Main.java
+++ src/main/java/it.fordprefect/Main.java
@@ -1,12 +1,24 @@
 package it.fordprefect;
 
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Whatever");
-        JFrame f = new JFrame();
-        f.setVisible(true);
+        Scanner s = new Scanner(System.in);
+
+        String testo = s.nextLine();
+        System.out.println(testo);
+        int tns =0;
+        System.out.println(testo.split("/").length);
+      String[] accposiz = testo.split("/");
+      for (int index = accposiz.length-4; index<accposiz.length; index += 1 ) {
+          System.out.println(index);
+
+
+
+      }
+
 
     }
 }
